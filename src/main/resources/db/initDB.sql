7e44803b7f78616f91e964b82ea58fc86229d80f
Ivan Matveev <matveevv1991@mail.ru>
2019-01-28 13:15:48 +0300

    add meals to init and populate DB scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,12 +1,15 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS meal_seq;
 
 CREATE SEQUENCE global_seq START 100000;
+create sequence meal_seq START 100000;
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id               INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
   name             VARCHAR                 NOT NULL,
   email            VARCHAR                 NOT NULL,
   password         VARCHAR                 NOT NULL,
@@ -23,3 +26,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+create table meals(
+  id integer primary key default nextval('global_seq'),
+  dateTime TIMESTAMP NOT NULL ,
+  description VARCHAR NOT NULL ,
+  calories INTEGER NOT NULL,
+  user_id INTEGER NOT NULL ,
+  foreign key (user_id) references users (id) on delete cascade
+);
+create index meals_dateTime_index on meals(dateTime);
+create index meals_user_id_index on meals(user_id);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE meal_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+
+INSERT into meals (datetime, description, calories, user_id) values
+('2019-01-02 10:00','Завтрак',500,100000),
+('2019-01-02 14:00','Обед',1000,100000),
+('2019-01-02 18:00','Ужин',500,100000),
+('2019-01-03 10:00','Завтрак',500,100000),
+('2019-01-03 14:00','Обед',1000,100000),
+('2019-01-03 18:00','Ужин',510,100000);
+
